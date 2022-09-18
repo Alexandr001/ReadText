6d67f4bcbed55cc9d7c88f768feed15ce695d740
Александр <a.a.popov2001@gmail.com>
2022-09-18 21:06:48 +0300

    Добавлены SQL апросы в коментарии

--- ReadText/Program.cs
+++ ReadText/Program.cs
@@ -8,3 +8,28 @@ const string PATH_NEW_FILE = @"NewFile.txt";
 IFile file = new File();
 string[] readText = file.ReadText(PATH_SOURCE_FILE);
 file.WriteText(readText, PATH_NEW_FILE);
+
+/*
+ 1------------------
+select * from Employee
+where salarey = 
+		(SELECT MAX(salarey) from Employee);
+ 2--------------------- вывести отдел с максимальной зп сотрудника
+select Dep.* from Depatment, Employee
+where salarey = 
+(SELECT MAX(salarey) from Employee)
+and Depatment.id = Employee.dep_id;
+ 3--------------------
+with sum_salary as
+( 
+select dep_id, sum(salarey) salarey from Employee
+group  by dep_id
+)
+select d.*, s.salarey from sum_salary s, Depatment d  
+where  s.salarey =
+( select max(salarey) from sum_salary )
+  and s.dep_id = d.id;
+ 4----------------------------------
+select * from Employee
+where [name] LIKE N'Р%н';
+*/
